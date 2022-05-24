bbfd225036e58d3ada98593651156d98020f6833
huangsheng <huangshengtx@163.com>
2022-05-24 22:49:19 +0800

    Update cut.py

--- cut.py
+++ cut.py
@@ -4,9 +4,9 @@ import xlwt
 from moviepy.editor import VideoFileClip
 import argparse
 import math
- 
+
 class FileUtils():
- 
+
     def __init__(self):
     	pass
     def get_filesize(self,filename):
@@ -15,7 +15,7 @@ class FileUtils():
         """
         file_byte = os.path.getsize(filename)
         return self.sizeConvert(file_byte)
- 
+
     def get_file_times(self,filename):
         """
         获取视频时长（s:秒）
@@ -23,7 +23,7 @@ class FileUtils():
         clip = VideoFileClip(filename)
         file_time = self.timeConvert(clip.duration)
         return file_time,clip.duration
- 
+
     def sizeConvert(self,size):# 单位换算
         K, M, G = 1024, 1024**2, 1024**3
         if size >= G:
@@ -34,7 +34,7 @@ class FileUtils():
             return str(size/K)+'K Bytes'
         else:
             return str(size)+'Bytes'
-    
+
     def timeConvert(self,size):# 单位换算
         M, H = 60, 60**2
         if size < M:
@@ -48,7 +48,7 @@ class FileUtils():
             tim_srt = u'%s小时%s分钟%s秒'%(hour,mine,second)
             return tim_srt
 
-def create_output_path(input,output):
+def create_output_path(input,output,cut_type):
     _filename = os.path.basename(input)
     filename, extension = os.path.splitext(_filename)
     output_dir = os.path.join(output,'sub-videos',"sub-"+filename)
@@ -56,13 +56,13 @@ def create_output_path(input,output):
     isExists=os.path.exists(output_dir)
     if not isExists:
         os.makedirs(output_dir)
-    base_output_file = 'sub{}-'+filename+extension
+    base_output_file = cut_type+'-sub{}-'+filename+extension
     return os.path.join(output_dir,base_output_file),filename
 
 def cut_by_duration_time(input,t,output='./'):
     file_util = FileUtils()
     _,s = file_util.get_file_times(input)
-    base_output_file,filename = create_output_path(input,output)
+    base_output_file,filename = create_output_path(input,output,"time")
     num = math.ceil( s / 30 )
     print("filename:{} length: {}".format(filename,s))
     print("cut video into {} subv, every subv {}s".format(num,t))
@@ -71,7 +71,7 @@ def cut_by_duration_time(input,t,output='./'):
 def cut_by_num_of_subvideo(input,n,output='./'):
     file_util = FileUtils()
     _,second = file_util.get_file_times(input)
-    base_output_file,filename = create_output_path(input,output)
+    base_output_file,filename = create_output_path(input,output,"num")
     sub_second = math.ceil( second / n )
     print("filename:{} length: {}".format(filename,second))
     print("cut video into {} subv, every subv {}s".format(n,sub_second))
@@ -92,11 +92,26 @@ def to_mp4(input,output='./'):
     output_dir = os.path.join(output,'conversed')
     isExists=os.path.exists(output_dir)
     if not isExists:
-        os.makedirs(output_dir)  
+        os.makedirs(output_dir)
 
     output_file = os.path.join(output_dir,filename+'-conv.mp4')
     print("====> save to ",output_file)
     os.system('ffmpeg -i {} {}'.format(input,output_file))
+
+#ffmpeg -i 2021.mp4 -metadata:s:v rotate="270" -codec copy 徐真真.mp4
+def rotate(r,input,output='./'):
+    file_util = FileUtils()
+    _,second = file_util.get_file_times(input)
+    _filename = os.path.basename(input)
+    filename, extension = os.path.splitext(_filename)
+    output_dir = os.path.join(output,'rotate')
+    isExists=os.path.exists(output_dir)
+    if not isExists:
+        os.makedirs(output_dir)
+    output_file = os.path.join(output_dir,filename+'.mp4')
+    print("====> save to ",output_file)
+    os.system('ffmpeg -i {} -metadata:s:v rotate="{}" -codec copy {}'.format(input,r,output_file))
+
 
 
 def merge(video_path,audio_path,output):
@@ -118,15 +133,16 @@ if __name__ == "__main__":
     parser.add_argument('--m', type=str,help='use which tool. [cut] for cut or [ts] for transcoding or [mg] for merge')
     parser.add_argument('--t', type=int,help='The length of each subvideo',default=30)
     parser.add_argument('--o', type=str,help='The path of the output',default='./')
-    parser.add_argument('--c', type=str,help='the approch of cut. [num]:cut_by_num_of_subvideo or [time]:cut_by_duration_time',default='num')
+    parser.add_argument('--c', type=str,help='the approch of cut. [num]:cut_by_num_of_subvideo or [time]:cut_by_duration_time')
     parser.add_argument('--n', type=int,help='num_of_subvideo',default=5)
     parser.add_argument('--a', type=str,help='the audio path')
+    parser.add_argument('--r', type=int,help='the rotate',default=90)
     args = parser.parse_args()
     if args.m:
         if args.m=='cut' and args.i:
             if args.c == 'time':
                 print("cut_by_duration_time: ")
-                cut_by_duration_time(args.i,args.t,args.o)           
+                cut_by_duration_time(args.i,args.t,args.o)
             elif args.c == 'num':
                 print("cut_by_num_of_subvideo")
                 cut_by_num_of_subvideo(args.i,args.n,args.o)
@@ -136,6 +152,8 @@ if __name__ == "__main__":
             to_mp4(args.i,args.o)
         elif args.m=='mg' and args.i and args.a:
             merge(args.i,args.a,args.o)
+        elif args.m=='ro' and args.i:
+            rotate(args.r,args.i,args.o)
         else:
             print("Please specify which tool to be use:  [c] for cut or [t] for transcoding")
             parser.print_help()
